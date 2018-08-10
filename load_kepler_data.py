ab1ebab8141790a3feb0caf6ef07120b66464e10
apmechev <apmechev@gmail.com>
2018-08-10 10:26:25 +0200

    loading data now creates a list of light curves and the data is conditioned for a periodogram

--- load_kepler_data.py
+++ load_kepler_data.py
@@ -4,14 +4,18 @@ import pickle
 
 def get_flux_for_star(KID):
     """Loads time series data of tuples of (Time (JD), Aperture Flux (e-/s), Flux_error)
-    given a star's Kepler ID"""
+    given a star's Kepler ID
+    returns a list of light curves (doesn't append them all together because of different
+    baselines, each curve has its own baseline)"""
     client = kplr.API()
     star = client.star("{}".format(KID))
     curves = star.get_light_curves(fetch=True, short_cadence=False)
     data=[]
     for curve in curves:
+        _data=[]
         data1=fits.open(curve.filename)[1].data
         for point in data1:
-            data.append((point[0],point[3],point[4])) #Time, flux, fluxerr
+            if point[7]>0:
+                _data.append((point[0],point[7],point[8])) #Time, flux, fluxerr
+        data.append(_data)
     return data
-
