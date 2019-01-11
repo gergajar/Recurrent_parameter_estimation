89e95946e62670ecf57b078d0500231bcb21edca
ReyesDeJong <reyes.dejong@gmail.com>
2019-01-11 16:12:34 -0500

    one class 40000

--- generate_dataset_pkl.py
+++ generate_dataset_pkl.py
@@ -63,7 +63,7 @@ if __name__ == "__main__":
                               amp_noise=amp_noise,
                               phase_noise=phase_noise)
 
-        n_examples = 100#37500
+        n_examples = 40000#37500
         set_prop = 0.8, 0.1, 0.1
 
         data.generate_dataset(set_prop=set_prop,
