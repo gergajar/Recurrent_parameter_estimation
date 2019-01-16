1af45d6187877d9b45a018b1b60f8f3821369594
ReyesDeJong <reyes.dejong@gmail.com>
2019-01-16 14:46:59 -0500

    pickle dump directily to harvardgan

--- generate_dataset_pkl.py
+++ generate_dataset_pkl.py
@@ -132,6 +132,8 @@ if __name__ == "__main__":
             sets[set_idx][k] = [d[k] for d in set_dict]
 
     pickle.dump([train, val, test], open(os.path.join(data_path, 'data_set.pkl'), "wb"), protocol=2)
+    pickle.dump([train, val, test], open(os.path.join(PATH_TO_PROJECT, '..', 'harvardgan', 'TOY' 'toy.pkl'), "wb"),
+                protocol=2)
     print('train shape %s' % str(np.array(train['original_magnitude']).shape))
     print('val shape %s' % str(np.array(val['original_magnitude']).shape))
     print('test shape %s' % str(np.array(test['original_magnitude']).shape))
